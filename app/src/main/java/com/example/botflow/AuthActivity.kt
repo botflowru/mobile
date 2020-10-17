64511e4c5d13fa3ad8cb0f344fcc92e90d467963
igo320341 <igo320341@gmail.com>
2020-10-17 23:04:43 +0300

    add vk auth

--- app/src/main/java/com/example/botflow/AuthActivity.kt
+++ app/src/main/java/com/example/botflow/AuthActivity.kt
@@ -3,8 +3,10 @@ package com.example.botflow
 import android.animation.ObjectAnimator
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
@@ -12,6 +14,10 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.SignInButton
 import com.google.android.gms.common.api.ApiException
+import com.vk.api.sdk.VK
+import com.vk.api.sdk.auth.VKAccessToken
+import com.vk.api.sdk.auth.VKAuthCallback
+import com.vk.api.sdk.auth.VKScope
 
 
 class AuthActivity : AppCompatActivity() {
@@ -27,12 +33,13 @@ class AuthActivity : AppCompatActivity() {
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
         val account = GoogleSignIn.getLastSignedInAccount(this)
         if (account != null) {
-            authorizedWithGoogle(account)
+            authorisedWithGoogle(account)
         }
         setContentView(R.layout.auth_activity)
         val logo = findViewById<ImageView>(R.id.logo_image)
         val authButtons = findViewById<LinearLayout>(R.id.auth_buttons)
         val signInButton = findViewById<SignInButton>(R.id.sign_in_button)
+        val vkAuthButton = findViewById<ImageButton>(R.id.vk_auth)
         ObjectAnimator.ofFloat(logo, "translationY", -500f).apply {
             duration = 2000
             start()
@@ -46,17 +53,32 @@ class AuthActivity : AppCompatActivity() {
             val signInIntent = mGoogleSignInClient.signInIntent;
             startActivityForResult(signInIntent, RC_SIGN_IN);
         }
+        vkAuthButton.setOnClickListener {
+            VK.login(this, arrayListOf(VKScope.WALL, VKScope.PHOTOS))
+        }
     }
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == RC_SIGN_IN) {
             val task = GoogleSignIn.getSignedInAccountFromIntent(data)
             val account = task.getResult(ApiException::class.java)
-            authorizedWithGoogle(account!!)
+            authorisedWithGoogle(account!!)
 
         }
+        val callback = object: VKAuthCallback {
+            override fun onLogin(token: VKAccessToken) {
+                authorisedWithVK(token)
+            }
+
+            override fun onLoginFailed(errorCode: Int) {
+                Toast.makeText(this@AuthActivity, "VK auth error", Toast.LENGTH_LONG).show()
+            }
+        }
     }
-    fun authorizedWithGoogle(acount: GoogleSignInAccount) {
+    private fun authorisedWithGoogle(account: GoogleSignInAccount) {
+        Toast.makeText(this, account.displayName, Toast.LENGTH_LONG).show()
+    }
+    private fun authorisedWithVK(token: VKAccessToken) {
 
     }
 }
